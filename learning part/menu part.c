a0b8fcd135bca0d5aadf07eac197d38a1df27916
sajjad-njr <sajjadrahman1020@gmail.com>
2020-09-17 21:01:38 +0600

    menu part use for loop

--- learning part/menu part.c
+++ learning part/menu part.c
@@ -9,11 +9,17 @@ void main()
 
 void menu()
 {
-    char*  productName[3] ={"Burger","Mango","Yango"} ;
-    int price[3] = {768,890,78} ;
+    char*  productName[5] ={"Burger","Mango","Yango","salad","alu chop"};
 
-    printf("\n\t%s\t%d\n",productName[0],price[0]);
+    int i,price[5] = {768,890,78,500,789} ;
+
+   /* printf("\n\t%s\t%d\n",productName[0],price[0]);
     printf("\n\t%s\t%d\n",productName[1],price[1]);
-    printf("\n\t%s\t%d\n",productName[2],price[2]);
+    printf("\n\t%s\t%d\n",productName[2],price[2]); */
+
+    for (i = 0 ; i < 5 ; i++)
+    {
+        printf("\n\t%s\t%d\n",productName[i],price[i]);
+    }
 }
 
