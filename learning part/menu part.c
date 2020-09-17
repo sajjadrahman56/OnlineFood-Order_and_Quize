36d2bb993c5c37e236b20c1f15d45185f236df16
sajjad-njr <sajjadrahman1020@gmail.com>
2020-09-17 22:45:33 +0600

    menu part product space qual

--- learning part/menu part.c
+++ learning part/menu part.c
@@ -21,20 +21,33 @@ void menu()
     int productLen = sizeof(productName)/4;
     int largeNum = 0;
 
-    for (int i = 0 ; i < 5 ; i++)
-    {
-        printf("\n\t%s\t%d\n",productName[i],price[i]);
-    }
 
-
-    for(int i =0  ; i <  productLen ; i++)
+    //get space for equal
+     for(int i =0  ; i <  productLen ; i++)
     {
         if(largeNum < strlen(productName[i]))
         {
             largeNum = strlen(productName[i]);
         }
     }
+
+
     //printf("Large name = %d\n",largeNum);
+
+    for (int i = 0 ; i < 5 ; i++)
+    {
+        printf("\n\t%s",productName[i]);
+
+        int longSpace = largeNum - strlen(productName[i])+10; //space equal
+        for (int j = 0 ; j < longSpace ; j++)
+        {
+            printf(" ");
+        }
+        printf("%d",price[i]);
+    }
+
+
+
 
 
 }
