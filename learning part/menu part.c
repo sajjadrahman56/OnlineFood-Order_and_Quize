d0e221c624882808610de838d2756db4bd712650
sajjad-njr <sajjadrahman1020@gmail.com>
2020-09-17 21:33:22 +0600

    menu part product len sucesse

--- learning part/menu part.c
+++ learning part/menu part.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 void menu();
 void main()
@@ -9,17 +10,32 @@ void main()
 
 void menu()
 {
-    char*  productName[5] ={"Burger","Mango","Yango","salad","alu chop"};
+    char*  productName[5] = {"Burger","Mango","Yango","salad","alu chop"};
 
     int i,price[5] = {768,890,78,500,789} ;
 
-   /* printf("\n\t%s\t%d\n",productName[0],price[0]);
-    printf("\n\t%s\t%d\n",productName[1],price[1]);
-    printf("\n\t%s\t%d\n",productName[2],price[2]); */
+    /* printf("\n\t%s\t%d\n",productName[0],price[0]);
+     printf("\n\t%s\t%d\n",productName[1],price[1]);
+     printf("\n\t%s\t%d\n",productName[2],price[2]); */
 
-    for (i = 0 ; i < 5 ; i++)
+    int productLen = sizeof(productName)/4;
+    int largeNum = 0;
+
+    for (int i = 0 ; i < 5 ; i++)
     {
         printf("\n\t%s\t%d\n",productName[i],price[i]);
     }
+
+
+    for(int i =0  ; i <  productLen ; i++)
+    {
+        if(largeNum < strlen(productName[i]))
+        {
+            largeNum = strlen(productName[i]);
+        }
+    }
+    //printf("Large name = %d\n",largeNum);
+
+
 }
 
