b83088193414b4db80b952997b63fabe44ab4a2e
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-13 14:23:12 +0600

    case 1 nested switch case done

--- 13-10-2020-/sava.c
+++ 13-10-2020-/sava.c
@@ -13,8 +13,21 @@ int main()
             system("cls");
             printf("\n\tSanjida I love you \n\n");
             printf("\n\t1.tour\t\t2.kabar\t\t3.Sports\n\n");
+
             int m;
-            printf("\n\tchose one whatever you want !!\n\n");
+            printf("\n\tchose one whatever you want !!\n");
+            scanf("%d",&m);
+
+            switch(m)
+            {
+            case 1:
+                printf("\nDhaka\n");
+                break;
+            case 2:
+                printf("\nJalkati\n");
+            default:
+                printf("\n\t\terror !! you limit our cross , go home");
+            }
             break;
     }
 
