eed2d242c1ad244b8360ba2da37a714783f1ce41
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-13 14:29:37 +0600

    c-1 nested switch while add

--- 13-10-2020-/sava.c
+++ 13-10-2020-/sava.c
@@ -9,26 +9,33 @@ int main()
 
     switch(n)
     {
-        case 1:
-            system("cls");
-            printf("\n\tSanjida I love you \n\n");
-            printf("\n\t1.tour\t\t2.kabar\t\t3.Sports\n\n");
+    case 1:
+        system("cls");
+        printf("\n\tSanjida I love you \n\n");
+        printf("\n\t1.tour\t\t2.kabar\t\t3.Sports\n\n");
 
-            int m;
-            printf("\n\tchose one whatever you want !!\n");
-            scanf("%d",&m);
+        int m,n;
+        printf("\n\tchose one whatever you want !!\n");
+        scanf("%d",&m);
 
-            switch(m)
+        while(m--)
+        {
+            printf("\n\t\tchose one   !!\n");
+            scanf("%d",&n);
+            switch(n)
             {
             case 1:
                 printf("\nDhaka\n");
                 break;
             case 2:
                 printf("\nJalkati\n");
+                break;
             default:
                 printf("\n\t\terror !! you limit our cross , go home");
             }
-            break;
+        };
+
+        break;
     }
 
 
