44e68d5e05dce62f78e66d3beb71b8db0dbd01ae
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-14 17:02:44 +0600

    for loop use less

--- 14-10-2020/sava2ndDay.c
+++ 14-10-2020/sava2ndDay.c
@@ -36,9 +36,9 @@ int dada()
         {
             printf("\t%d. = %s\t\t",i+1,name[i]);
         }
-        char *naturePark[]={"Ratargul Swamp Forest","Bisnakandi","Jaflong","Lalakhal","Khadimnagar National Park","Tilagor Eco Park","Sreemangol Tea Garden","Madavkundo","Malnichara Tea state","Lakkatura Tea Garden"};
+        char *naturePark[]= {"Ratargul Swamp Forest","Bisnakandi\t\t","Jaflong\t\t","Lalakhal\t\t","Khadimnagar National Park","Tilagor Eco Park\t","Sreemangol Tea Garden","Madavkundo\t\t","Malnichara Tea state","Lakkatura Tea Garden"};
 
-        char *others[] ={"Shahi Edgah","Shajalal Mazar","Surma Bridge","Kadir Bazar Bridge","King Bridge and Ali Amzad clock","Pantumai Waterfall","Rema-kalenga Wildlife","Adventure World","Osmani Museum","Osmani Park","Dreamlend","Jakariya City"};
+        char *others[] = {"Shahi Edgah","Shajalal Mazar","Surma Bridge","Kadir Bazar Bridge","King Bridge and Ali Amzad clock","Pantumai Waterfall","Rema-kalenga Wildlife","Adventure World","Osmani Museum","Osmani Park","Dreamlend","Jakariya City"};
         while(m--)
         {
             printf("\n\n\t\tchose one !!\n");
@@ -47,15 +47,21 @@ int dada()
             {
             case 1:
                 //char *naturePark[]={"Ratargul Swamp Forest","Bisnakandi","Jaflong","Lalakhal","Khadimnagar National Park","Tilagor Eco Park","Sreemangol Tea Garden","Madavkundo","Malnichara Tea state","Lakkatura Tea Garden"};
-                for(int k = 0 ; k < 11; k++)
+                for(int k = 0 ; k < 12; k++)
                 {
                     printf("\n\t%d = %s\n",k+1,naturePark[k]);
                 }
                 break;
-            case 2:
 
-                printf("\nRatargu\n");
+            case 2:
+                /*for(int o = 0 ; o < 12; o++)
+                {
+                    printf("\n\t%d = %s\t\t%d = %s\n",o+1,naturePark[o],o+2,naturePark[o+1]);
+                }*
+                printf("\n");
+
                 break;
+
             default:
                 printf("\n\t\terror !! you limit our cross , go home");
             }
@@ -93,24 +99,25 @@ int dada()
                                "Neymar           ",
                                "Messi            ",
                                "Mabppe           ",
-                               "Lawandoski       "};
-                int salary[5]={90,65,43,23,12};
+                               "Lawandoski       "
+                              };
+                int salary[5]= {90,65,43,23,12};
                 printf("\n\t\tNo \t\tname\t\tsalary(million $)\n");
-                for(m = 0 ; m <5 ;m++)
+                for(m = 0 ; m <5 ; m++)
                 {
                     printf("\n\t\t%d\t %s\t\t%d",m+1,nam[m],salary[m]);
                 }
 
                 break;
             case 2:
-                 printf("\n\t\t*****************************\n");
+                printf("\n\t\t*****************************\n");
                 printf("\n\t\t***** Top 10 Footballer *****\n");
                 printf("\n\t\t*****************************\n");
 
                 char *Movinam[] = {"Cristiano Ronaldo","Neymar","Messi","Mabppe","Lawandoski"};
-                int pricedam[5]={90,65,43,23,12};
+                int pricedam[5]= {90,65,43,23,12};
                 printf("\n\t\tNo \t\tname");
-                for(m = 0 ; m <5 ;m++)
+                for(m = 0 ; m <5 ; m++)
                 {
                     printf("\n\t\t%d\t %s",m+1,nam[m]);
                 }
