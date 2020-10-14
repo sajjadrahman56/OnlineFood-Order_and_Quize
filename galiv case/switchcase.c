27f421b1f73dd1de49079399e74bb6fe079b330e
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-14 20:44:30 +0600

    switch case 35

--- galiv case/switchcase.c
+++ galiv case/switchcase.c
@@ -265,4 +265,23 @@ case 34 :
                 p[order_count].cost += (160 * qu);
             break;
         }
+case 35 :
+        {
+            printf ("Enter the size of  Naan: 1. half  2.full\n");
+            int i;
+            scanf ("%d", &i);
+            getchar();
 
+            printf ("Please enter the quantity: ");
+            scanf ("%d", &qu);
+            getchar();
+
+            p[order_count].number_of_food += qu;
+
+            if (i==1)
+                p[order_count].cost += (15 * qu);
+
+            else
+                p[order_count].cost += (25 * qu);
+            break;
+        }
