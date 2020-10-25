a90f10294af8241ce25e603643c02bcda44bdeb7
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-25 23:07:33 +0600

    c++ division

---
+++ 25-10-2020/divided.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std ;
+
+
+int main()
+{
+  int a , b ;
+
+  cout << "Enter two number ";
+
+  cin >> a >> b ;
+
+  cout << "Divison = " << a /b ;
+
+  return 0 ;
+}
