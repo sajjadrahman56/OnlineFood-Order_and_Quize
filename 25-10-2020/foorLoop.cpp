d683892b93a116219c937457b206ca61a71ca3d4
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-26 04:10:42 +0600

    c++ divison

--- 25-10-2020/foorLoop.cpp
+++ 25-10-2020/foorLoop.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
 
+using namespace std ;
+
+int main()
+{
+    int a , n = 10 ;
+
+    cout<<"Foor loop work ";
+
+    cin>> a ;
+
+    cout << "\n\tPrintf you are succesfully : \n";
+
+    for ( int i = 0 ; i < 5 ; i++)
+    {
+        cout<< "\n\tPrintf %d\t = %d "  << i << i * 9 ;
+    }
+
+    return 0 ;
+}
