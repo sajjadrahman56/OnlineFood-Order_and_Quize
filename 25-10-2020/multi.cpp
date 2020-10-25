a96335450e62970587bd85e2fb12559d8ec26308
sajjad-njr <sajjadrahman1020@gmail.com>
2020-10-25 23:01:00 +0600

    c++ multiple

---
+++ 25-10-2020/multi.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int a , b , c ;
+
+    cout << "Enter three nb ";
+
+    cin >> a >> b >> c ;
+
+    cout << "Print add = " << a*b*c ;
+
+    return 0 ;
+
+}
